dbcb954228635ee0dc06d4df7bf191b02a797ffa
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-29 11:29:27 +0600

    Display form data in a table and form error handle

--- src/Components/ProductManagement/ProductForm.jsx
+++ src/Components/ProductManagement/ProductForm.jsx
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useState } from "react";
+const ProductForm = ({ handleAddProduct }) => {
+  const [error, setError] = useState("");
 
-const ProductForm = () => {
+  const handleProductSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const price = e.target.price.value;
+    const quantity = e.target.quantity.value;
 
-    const handleProductSubmit = (e) => {
-        e.preventDefault();
-        const name = e.target.name.value;
-        const price = e.target.price.value;
-        const quantity = e.target.quantity.value;
-
-        console.log(name, price, quantity);
-
-        const newProduct = {
-            name,
-            price,
-            quantity
-        }
-        console.log(newProduct);
-
+    // console.log(name, price, quantity);
+    //validation
+    if (name.length === 0) {
+      setError("Name is required");
+      return;
+    } else if (price.length === 0) {
+      setError("Price is required");
+      return;
+    } else if (price < 0) {
+      setError("Price cannot be negative");
+      return;
+    } else if (quantity.length === 0) {
+      setError("Quantity is required");
+      return;
+    } else if (quantity < 0) {
+      setError("Quantity cannot be negative");
+      return;
+    } else {
+      setError("");
     }
 
-    return (
-      <div>
-        <h3>Add a Product</h3>
-        <form onSubmit={handleProductSubmit}>
-          <input type="text" name="name" placeholder="Product Name" />
-          <br />
-          <input type="text" name="price" placeholder="Product Price" />
-          <br />
-          <input type="text" name="quantity" placeholder="Product Quantity" />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
+    const newProduct = {
+      name,
+      price,
+      quantity,
+    };
+    // console.log(newProduct);
+    handleAddProduct(newProduct);
+  };
+
+  return (
+    <div>
+      <h3>Add a Product</h3>
+      <form onSubmit={handleProductSubmit}>
+        <input type="text" name="name" placeholder="Product Name" />
+        <br />
+        <input type="text" name="price" placeholder="Product Price" />
+        <br />
+        <input type="text" name="quantity" placeholder="Product Quantity" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <p style={{ color: "red" }}>
+        <small>{error}</small>
+      </p>
+    </div>
+  );
 };
 
 export default ProductForm;
