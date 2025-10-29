36365e0ce891e86c7ad70638277540a75b4f872b
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-29 18:33:13 +0600

    Create a Family Component Tree structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import ControlledField from "./Components/ControlledField/ControlledField";
 import HookForm from "./Components/ControlledField/HookForm/HookForm";
+import FamilyTree from "./Components/Family Tree/FamilyTree";
 import FormAction from "./Components/FormAction/FormAction";
 import ProductManagement from "./Components/ProductManagement/ProductManagement";
 import SimpleForm from "./Components/SimpleForm/SimpleForm";
@@ -15,7 +16,8 @@ function App() {
       {/* <ControlledField></ControlledField> */}
       {/* <UnControlledField></UnControlledField> */}
       {/* <HookForm></HookForm> */}
-      <ProductManagement></ProductManagement>
+      {/* <ProductManagement></ProductManagement> */}
+      <FamilyTree></FamilyTree>
     </>
   );
 }
